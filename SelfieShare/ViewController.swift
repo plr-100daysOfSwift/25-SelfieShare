cfd571ec70d937a2be1bd8b6db3d9c17612c05e8
Paul Richardson <developer@photographerafoot.com>
2021-06-08 08:52:53 +1000

    Initial Commit

---
+++ SelfieShare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SelfieShare
+//
+//  Created by Paul Richardson on 08/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
